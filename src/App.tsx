1b3a7738dc34bea4fcd12c19ff4b6bbbef27945f
Nizam <sardernizam1@gmail.com>
2019-09-01 15:50:40 +0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from "@blueprintjs/core";
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mw9 center ph3-ns">
+        <div className="cf ph2-ns">
+          <div className="fl w-100 w-50-ns pa2">
+            <h1 className="f6 f3-m f1-l lh-copy">Title</h1>
+            <Button icon="refresh" />
+            <Button intent="success" text="button content" />
+          </div>
+          <div className="fl w-100 w-50-ns pa2">
+            <h1 className="f4 lh-copy">description</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
